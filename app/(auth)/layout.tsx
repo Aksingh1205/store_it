068f260aa6836430885cd38fef6ecea1da5c5481
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-02-03 20:13:35 +0530

    Updated layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -19,9 +19,28 @@ const layout = ({children} : {children: React.ReactNode}) => {
                         This is a place where you can store all your documents.
                     </p>
                 </div>
+                <Image
+                    src="/assets/images/files.png"
+                    alt="Files"
+                    width={342}
+                    height={342}
+                    className="transition-all hover:rotate-2 hover:scale-105"
+                />
             </div>
         </section>
+
+        <section className='flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0'>
+            <div className='mb-16 lg:hidden'>
+            <Image
+                src="/assets/icons/logo-full-brand.svg"
+                alt="logo"
+                width={224}
+                height={82}
+                className="h-auto w-[200px] lg:w-[250px]"
+            />
+            </div>
         {children}
+        </section>
     </div>
   )
 }

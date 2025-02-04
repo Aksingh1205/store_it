8c24f677097d84cf8be70741aa8bb926d2df7e09
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-02-04 22:05:44 +0530

    Setup Authform in signin and signup pages

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import AuthForm from "@/components/AuthForm"
 
-const SignIn = () => {
-  return (
-    <div>
-        Sign In
-    </div>
-  )
-}
+// AuthForm is a reusable component that can handle different types of authentication forms (e.g., sign-in, sign-up, password reset).
+// The type prop is used to configure the AuthForm component to display the correct form fields and behavior for the specified authentication type.
+const SignIn = () => <AuthForm type ="sign-in" />
 
 export default SignIn

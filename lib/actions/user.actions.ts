a24baa6849ecd4d1efa67ed19cb3fcc6715cbf16
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-02-12 23:15:25 +0530

    Updated user actions
    write logic for searching existing user

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,4 +1,21 @@
 'use server';
+
+import { Query } from "node-appwrite";
+import { appwriteConfig } from "../appwrite/config";
+import { createAdminClient } from "../appwrite";
+
+const getUserByEmail = async(email: string) => {
+    const {databases} = await createAdminClient();
+
+    const result = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectionId,
+        [Query.equal("email", [email])],
+    );
+
+    return result.total > 0 ? result.documents[0] : null;
+}
+
 const createAccount = async({fullName, email} : {fullName: string; email: string;}) => {
-    
+    const existingUser = await getUserByEmail(email);
 }

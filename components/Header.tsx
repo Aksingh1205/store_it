95c5b33adb3026bc653907675794c9ae3bd62bff
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-02-18 20:31:34 +0530

    Created Search and fileuploader component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
+import Search from './Search'
+import FileUploader from './FileUploader'
 
 const Header = () => {
   return (
     <div className='header'>
-      Search
+      <Search />
       <div className='header-wrapper'>
-        FileUploader
+        <FileUploader />
         <form>
           <Button type='submit' className='sign-out-button'>
             <Image

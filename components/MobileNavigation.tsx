820df25908bc99322472a9ee475ddb6dadde1d7e
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-02-26 21:07:50 +0530

    Sheet component is edited and props are passed

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import {
   Sheet,
   SheetContent,
@@ -8,9 +10,28 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet"
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
+import { Separator } from './ui/separator'
 
+interface Props {
+  $id: string;
+  accountId: string;
+  fullName: string;
+  avatar: string;
+  email: string;
+}
 
-const MobileNavigation = () => {
+const MobileNavigation = ({
+  $id: ownerId,
+  accountId,
+  fullName,
+  avatar,
+  email,
+}: Props) => {
+
+  const [open, setOpen] = useState(false)
+  const pathname = usePathname()
+
   return (
     <header className="mobile-header">
       <Image
@@ -20,11 +41,33 @@ const MobileNavigation = () => {
         height={52}
         className="h-auto"
       />
-      <Sheet>
-        <SheetTrigger>Open</SheetTrigger>
-        <SheetContent>
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+        <Image
+            src="/assets/icons/menu.svg"
+            alt="Search"
+            width={30}
+            height={30}
+          />
+        </SheetTrigger>
+        <SheetContent className='shad-sheet h-screen px-3'>
           <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetTitle>
+            <div className="header-user">
+              <Image
+                src={avatar}
+                alt="avatar"
+                width={44}
+                height={44}
+                className="header-user-avatar"
+              />
+              <div className="sm:hidden lg:block">
+                <p className="subtitle-2 capitalize">{fullName}</p>
+                <p className="caption">{email}</p>
+              </div>
+              </div>
+              <Separator className="mb-4 bg-light-200/20" />
+            </SheetTitle>
             <SheetDescription>
               This action cannot be undone. This will permanently delete your account
               and remove your data from our servers.

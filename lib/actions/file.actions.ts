01e7343bf7325ab96e1ad6fcef1d4e1bd94de24b
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-03-06 23:27:02 +0530

    file stored in database
    file stored in database via creating new document in database.

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -4,7 +4,8 @@ import { createAdminClient } from "../appwrite";
 import { InputFile } from "node-appwrite/file";
 import { appwriteConfig } from "../appwrite/config";
 import { ID } from "node-appwrite";
-import { constructFileUrl, getFileType } from "../utils";
+import { constructFileUrl, getFileType, parseStringify } from "../utils";
+import { revalidatePath } from "next/cache";
 
 const handleError = (error : unknown, message : string) => {
     console.error(error, message);
@@ -33,7 +34,21 @@ export const uploadFile = async ({file, ownerId, accountId, path}: UploadFilePro
             accountId,
             users: [],
             bucketFileId: bucketFile.$id,
-        }
+        };
+
+        const newFile = await databases
+            .createDocument(
+                    appwriteConfig.databaseId,
+                    appwriteConfig.filesCollectionId,
+                    ID.unique(),
+                    fileDocument,
+            )
+            .catch(async (error: unknown) => {
+                await storage.deleteFile(appwriteConfig.bucketId, bucketFile.$id);
+                handleError(error, "Failed to create file document");
+            });
+        revalidatePath(path);
+        return parseStringify(newFile);
     } catch (error) {
         handleError(error, "Failed to upload file");
     }

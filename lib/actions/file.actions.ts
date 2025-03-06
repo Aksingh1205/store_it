06db2bcc13c5e5e61146b9e088b5d004998b871b
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-03-06 23:16:35 +0530

    storing meta data of file stored in bucket
    storing meta data and file info stored in bucket while uploading in database via appwrite database feature.

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -4,6 +4,7 @@ import { createAdminClient } from "../appwrite";
 import { InputFile } from "node-appwrite/file";
 import { appwriteConfig } from "../appwrite/config";
 import { ID } from "node-appwrite";
+import { constructFileUrl, getFileType } from "../utils";
 
 const handleError = (error : unknown, message : string) => {
     console.error(error, message);
@@ -21,6 +22,18 @@ export const uploadFile = async ({file, ownerId, accountId, path}: UploadFilePro
             ID.unique(),
             inputFile
         );
+
+        const fileDocument = {
+            type: getFileType(bucketFile.name).type,
+            name: bucketFile.name,
+            url: constructFileUrl(bucketFile.$id),
+            extension: getFileType(bucketFile.name).extension,
+            size: bucketFile.sizeOriginal,
+            owner: ownerId,
+            accountId,
+            users: [],
+            bucketFileId: bucketFile.$id,
+        }
     } catch (error) {
         handleError(error, "Failed to upload file");
     }

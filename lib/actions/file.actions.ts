f0978302510bee4be1d7a60be070ce09bc3f828d
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-03-06 22:57:37 +0530

    file actions updated
    read file from buffer and upload to storage bucket

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -1,7 +1,28 @@
 'use server'
 
+import { createAdminClient } from "../appwrite";
+import { InputFile } from "node-appwrite/file";
+import { appwriteConfig } from "../appwrite/config";
+import { ID } from "node-appwrite";
+
+const handleError = (error : unknown, message : string) => {
+    console.error(error, message);
+    throw error;
+}
 
 export const uploadFile = async ({file, ownerId, accountId, path}: UploadFileProps) => {
+    const {storage, databases} = await createAdminClient();
 
+    try {
+        const inputFile = InputFile.fromBuffer(file, file.name);
+
+        const bucketFile = await storage.createFile(
+            appwriteConfig.bucketId,
+            ID.unique(),
+            inputFile
+        );
+    } catch (error) {
+        handleError(error, "Failed to upload file");
+    }
 
 }

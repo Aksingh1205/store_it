846b4854140be5d141982bd826ed5d0cd2338421
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-03-19 20:11:54 +0530

    ActionDropDown component updated
    dropdown and dialog box compnent added

---
+++ components/ActionDropDown.tsx
@@ -0,0 +1,47 @@
+'use client'
+import {
+    Dialog,
+  } from "@/components/ui/dialog"
+  
+  import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+import Image from "next/image"
+import { useState } from "react"
+  
+
+const ActionDropDown = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+        <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+        <DropdownMenuTrigger className="shad-no-focus">
+            <Image
+                src="/assets/icons/dots.svg"
+                alt="dots"
+                width={34}
+                height={34}
+            />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuLabel className="max-w-[200px] truncate">{file.name}</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+        </DropdownMenu>
+    </Dialog>
+
+  )
+}
+
+export default ActionDropDown

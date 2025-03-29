ccbfe8ad6f0e2021db92b70876beb09e6b514348
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-03-29 18:33:46 +0530

    Details section updated
    UI updated as Thumbnail added with name and date/time

--- components/ActionDropDown.tsx
+++ components/ActionDropDown.tsx
@@ -27,6 +27,7 @@ import { Input } from "./ui/input"
 import { Button } from "./ui/button"
 import { usePathname } from "next/navigation"
 import { renameFile } from "@/lib/actions/file.actions"
+import { FileDetails } from "./ui/ActionsModalContent"
   
 
 const ActionDropDown = ({ file }: { file: Models.Document }) => {
@@ -80,6 +81,7 @@ const ActionDropDown = ({ file }: { file: Models.Document }) => {
                         onChange={(e) => setName(e.target.value)}
                     />
                 )}
+                {value && 'details' && <FileDetails file={file} />}
                 </DialogHeader>
                 {['rename', 'delete', 'share'].includes(value) && (
                     <DialogFooter className="flex flex-col gap-3 md:flex-row">

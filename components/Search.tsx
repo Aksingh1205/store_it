280dc214ff71b792dc316cd006fd8dcc87161f5a
Aksingh1205 <arpitkumarsingh1205@gmail.com>
2025-04-05 23:46:25 +0530

    Fetch file function in useEffect

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,11 +1,31 @@
 'use client'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from './ui/input'
+import { useSearchParams } from 'next/navigation'
+import { getFiles } from '@/lib/actions/file.actions';
+import { Models } from 'node-appwrite'
 
 const Search = () => {
 
   const [query, setQuery] = useState('')
+  const searchParams = useSearchParams()
+  const searchQuery = searchParams.get('query') || ''
+  const [results, setResults] = useState<Models.Document>([])
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      const files = await getFiles({ searchText: query })
+    }
+
+    fetchFiles()
+  }, [third])
+  
+  useEffect(() => {
+    if(!searchQuery){
+      setQuery('')
+    }
+  }, [searchQuery])
   return (
     <div className='search'>
       <div className='search-input-wrapper'>
